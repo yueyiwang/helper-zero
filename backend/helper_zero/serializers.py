1d3cd3656c31221a6c76a1a3a985f1e18f27b55d
Jiefu Gong <jeff.gong@affirm.com>
2020-03-28 14:58:39 -0700

    Fix typo in serializers.py

--- backend/helper_zero/serializers.py
+++ backend/helper_zero/serializers.py
@@ -6,16 +6,14 @@ class UserSerializer(serializers.ModelSerializer):
 		model = User
 		fields = ('id', 'name', 'phone', 'email', 'zipcode', 'lat', 'lon')
 
-class OrganizationSerializer(serializers.ModelSerializer):
-    donation_requests = DonationRequestSerializer(many=True)
+class DonationRequestSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Organization
-        fields = ('id', 'name', 'phone', 'org_type', 'email', 'is_dropoff_only',
-                  'instructions', 'zipcode', 'lat', 'lon', 'donation_requests')
+        model = DonationRequest
+        fields = ('org_id', 'item_type', 'amount_requested', 'amount_received')
 
 class OrganizationSerializer(serializers.ModelSerializer):
     donation_requests = DonationRequestSerializer(many=True)
     class Meta:
         model = Organization
         fields = ('id', 'name', 'phone', 'org_type', 'email', 'is_dropoff_only',
-        		  'instructions', 'zipcode', 'lat', 'lon', 'donation_requests')
+                  'instructions', 'zipcode', 'lat', 'lon', 'donation_requests')
