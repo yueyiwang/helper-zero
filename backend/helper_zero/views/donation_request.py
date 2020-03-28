5f37ae6e341ff1bd3a9354352361018546ff9788
vicky11z <vicky11zhang@gmail.com>
2020-03-28 15:53:13 -0700

    add views and serializers for donation & donation_request

---
+++ backend/helper_zero/views/donation_request.py
@@ -0,0 +1,30 @@
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+
+from helper_zero.serializers import DonationRequestSerializer
+from helper_zero.models import DonationRequest, Organization
+
+class DonationRequestView(viewsets.ModelViewSet):
+    serializer_class = DonationRequestSerializer
+    queryset = DonationRequest.objects.all()
+
+    def create(self, request):
+        request_dict = request.data
+        serializer = DonationRequestSerializer(data=request_dict)
+        if serializer.is_valid():
+            if request_dict['amount_received'] is None:
+                request_dict["amount_received"] = 0
+            org = Organization.objects.get(id=request_dict["org_id"])
+            donation_request = DonationRequest(
+                org_id=org,
+                item_type=request_dict["item_type"],
+                amount_requested=request_dict["amount_requested"],
+                amount_received=request_dict["amount_received"],
+            )
+            donation_request.save()
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data)
+        
+
