d00571e224596492f4e03dbc9aa7afc031f35b8d
vicky11z <vicky11zhang@gmail.com>
2020-03-28 13:52:31 -0700

    remove extraneous null=true

--- backend/helper_zero/models.py
+++ backend/helper_zero/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Organization(models.Model):
-  name = models.CharField(max_length=120, null=True)
-  phone = models.CharField(max_length=120, null=True)
-  org_type = models.CharField(max_length=120, null=True)
-  email = models.EmailField(null=True)
+  name = models.CharField(max_length=120)
+  phone = models.CharField(max_length=120)
+  org_type = models.CharField(max_length=120)
+  email = models.EmailField()
   is_dropoff_only = models.BooleanField(default=True)
   instructions = models.TextField(blank=True)
   zipcode = models.CharField(blank=True, null=True, max_length=120)
@@ -33,12 +33,12 @@ class Donation(models.Model):
             'Organization',
             on_delete=models.CASCADE,
            )
-  status = models.CharField(max_length=120, null=True)
-  item_type = models.CharField(max_length=120, null=True)
+  status = models.CharField(max_length=120)
+  item_type = models.CharField(max_length=120)
   amount = models.PositiveIntegerField()
   created_at = models.DateTimeField()
   donation_time_start = models.DateTimeField()
-  donation_time_end = models.DateTimeField(null=True)
+  donation_time_end = models.DateTimeField()
 
   def _str_(self):
     return self.name
