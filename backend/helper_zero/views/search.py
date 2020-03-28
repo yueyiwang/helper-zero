29704d9d4aa3ec6c0a8d248e97b0e50d9dbf2733
Annie Wang <anniew@yelp.com>
2020-03-28 14:16:16 -0700

    refactor org search results, sort results by closest distance to serach loc, add pagination, add default search behavior

--- backend/helper_zero/views/search.py
+++ backend/helper_zero/views/search.py
@@ -1,10 +1,9 @@
 from rest_framework import status, viewsets
 from helper_zero.serializers import OrganizationSerializer, DonationRequestSerializer
 from helper_zero.models import Organization
-from helper_zero.models import DonationRequest
+from django.core.paginator import Paginator
 from rest_framework.response import Response
 from helper_zero.location_util import get_search_bounding_box
-from collections import defaultdict
 import math
 
 
@@ -18,42 +17,31 @@ class SearchView(viewsets.ViewSet):
         item_type = request.query_params.get('item_type')
         lat = request.query_params.get('lat')
         lon = request.query_params.get('lon')
-        lat = float(lat) if lat else None
-        lon = float(lon) if lon else None
         radius = request.query_params.get('radius', 10)
         zipcode = request.query_params.get('zipcode')
+        offset = int(request.query_params.get('offset', 0))
 
+        lat = float(lat) if lat else None
+        lon = float(lon) if lon else None
         delivery_values = [True] if delivery else [True, False]
-        org_list = _get_ordered_org_list(
-            zipcode, lat, lon, radius, delivery_values
-        )
-        donation_request_list = _get_donation_request_list(org_list, item_type) if org_list else []
 
-        response = _build_search_response(org_list, donation_request_list)
+        org_search_results = _get_org_search_results(
+            zipcode, lat, lon, radius, delivery_values, offset
+        )
+        response = _build_search_response(org_search_results, item_type)
         return Response(response)
 
 
-def _build_search_response(org_list, donation_request_list):
-    # if not org_list or not donation_request_list:
-    #     return []
-
-    org_id_to_donation_requests = defaultdict(list)
-    for donation_request in donation_request_list:
-        org_id = donation_request['org_id']
-        org_id_to_donation_requests[org_id] += donation_request
-
+def _build_search_response(org_list_results, item_type):
     response = []
-    for org in org_list:
-        org_id = org['id']
-        response_info = dict()
-        response_info['org'] = org
-        response_info['donation_requests'] = org_id_to_donation_requests[org_id]
-        response += [response_info]
-    print(response)
+    for org in org_list_results:
+        if item_type:
+            org['donation_requests'] = list(filter(lambda x: (x['item_type'] == item_type), org['donation_requests']))
+        response += [org]
     return response
 
 
-def _get_ordered_org_list(zipcode, lat, lon, radius, delivery_values):
+def _get_org_search_results(zipcode, lat, lon, radius, delivery_values, offset):
     if zipcode:
         org_query_set = Organization.objects.filter(
             zipcode=zipcode,
@@ -69,24 +57,33 @@ def _get_ordered_org_list(zipcode, lat, lon, radius, delivery_values):
             is_dropoff_only__in=delivery_values,
         ).order_by('name')
     else:
-        org_query_set = Organization.objects.order_by('name')[:20]
+        # If no location data, default to showing any orgs that have open requests, capping at 20
+        org_query_set = Organization.objects.filter(
+            donation_requests__isnull=True
+        )[:20]
 
-    org_list = OrganizationSerializer(org_query_set, many=True).data
-    sorted(org_list, key=lambda x: _get_distance(float(x['lat']), float(x['lon']), lat, lon))
-    return org_list
+    org_search_results = OrganizationSerializer(org_query_set, many=True).data
+    if lat and lon:
+        # Order by closest to search location
+        org_search_results.sort(key=lambda x: _get_distance(float(x['lat']), lat, float(x['lon']), lon))
+
+    org_search_results = _paginate_results(org_search_results, offset)
+    return org_search_results
 
 
-def _get_donation_request_list(org_list, item_type):
-    org_id_list = [org['id'] for org in org_list]
-    donation_request_query_set = DonationRequest.objects.filter(
-        org_id__in=org_id_list,
-        item_type=item_type,
-    )
-    donation_request_list = DonationRequestSerializer(donation_request_query_set, many=True).data
-    return donation_request_list
+def _paginate_results(org_search_results, offset):
+    paginator = Paginator(org_search_results, 20)  # Show 20 orgs per page
+    page_index = offset + 1
+    if page_index > 1:
+        if paginator.page(page_index-1).has_next():
+            # make sure we have another page of results
+            return paginator.page(page_index)
+        else:
+            return []
+    return org_search_results
 
 
 def _get_distance(x1, x2, y1, y2):
     if x1 and x2 and y1 and y2:
-        return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        return math.sqrt((x2 - x1) ** 2.0 + (y2 - y1) ** 2.0)
     return float("inf")
