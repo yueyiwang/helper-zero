e08447bd5e38bf92d391701d10bc29c69ebac6c4
vicky11z <vicky11zhang@gmail.com>
2020-03-29 11:01:39 -0700

    add validation for getting org list

--- backend/helper_zero/views/auth.py
+++ backend/helper_zero/views/auth.py
@@ -14,7 +14,10 @@ class AuthView(viewsets.ViewSet):
     if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
       return Response(status=status.HTTP_401_UNAUTHORIZED)
     user_id = id_info['sub']
-    org = Organization.objects.filter(auth_user_id=user_id).first()
+    org_list = Organization.objects.filter(auth_user_id=user_id)]
+    if len(org_list) != 1:
+      return Response(status=status.HTTP_400_BAD_REQUEST)
+    org = org_list.first()
     if org == None:
       return Response({})
     org_dict = OrganizationSerializer(org).data
