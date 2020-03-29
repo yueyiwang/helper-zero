b67c677d0fc4b6e06c7551994232a261398f6d1d
vicky11z <vicky11zhang@gmail.com>
2020-03-29 00:09:33 -0700

    get rid of comments

--- frontend/src/components/OrganizationSignUpPage/OrganizationSignUpPage.tsx
+++ frontend/src/components/OrganizationSignUpPage/OrganizationSignUpPage.tsx
@@ -1,128 +1,150 @@
-import React from "react";
-import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
-import {
-  Divider,
-  Button,
-  FormControl,
-  Input,
-  FormHelperText
-} from "@material-ui/core";
-import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import PeopleOutlined from "@material-ui/icons/PeopleOutlined";
-import DriveEtaOutlined from "@material-ui/icons/DriveEtaOutlined";
-import TextField from "@material-ui/core/TextField";
-import Header from "../Header";
+import React, { useState, useEffect } from "react";
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import PeopleOutlined from '@material-ui/icons/PeopleOutlined';
+import DriveEtaOutlined from '@material-ui/icons/DriveEtaOutlined';
+import GoogleLogin from 'react-google-login';
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+
+import { OrganizationType } from '../../types/OrganizationType';
+
+
 
 const styles: { [key: string]: React.CSSProperties } = {
-  container: {
-    display: "flex"
-  },
-  contentContainer: {
-    marginTop: "128px",
-    width: "100%"
-  },
   icon: {
-    verticalAlign: "bottom"
+    verticalAlign: "bottom",
+  },
+  buttonBox: {
+    width: "50%",
+    display: "block",
+  },
+  button: {
+    marginBottom: "20px",
   }
 };
 
 export default function ReceiverSignUpPage() {
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const [organization, setOrganization] = useState<OrganizationType>();
+  const [navigateToCreation, setNavigateToCreation] = useState<boolean>(false);
+  const [authToken, setAuthToken] = useState<String>('');
+
+  const onLoginSuccess = (response) => {
+    const auth_token = response.uc.id_token;
+    axios.get(`/api/login?auth_token=${auth_token}`).then(res => {
+      if (res.status !== 200) {
+        // Prompt user to create an account if their account does not exist.
+        setNavigateToCreation(true);
+      }
+      const retrievedOrg: OrganizationType = {
+        id: res.data.id,
+        name: res.data.name,
+        url: res.data.url,
+        address: res.data.address,
+        description: res.data.description,
+        phone: res.data.phone,
+        org_type: res.data.org_type,
+        email: res.data.email,
+        is_dropoff_only: res.data.is_dropoff_only,
+        instructions: res.data.instructions,
+        auth_user_id: res.data.auth_user_id,
+        donation_requests: res.data.donation_requests,
+        donations: res.data.donations,
+      }
+      setOrganization(retrievedOrg);
+      setLoggedIn(true);
+    })
+  }
+
+  const onLoginFailure = (response) => {
+    // TODO: navigate to error page
+    return;
+  }
+
+  const onCreateAccountSuccess = (response) => {
+    const authToken = response.uc.id_token;
+    setAuthToken(authToken);
+    setNavigateToCreation(true);
+  }
+
+  const onCreateAccountFailure = (response) => {
+    // TODO: navigate to error page
+    setNavigateToCreation(true);
+  }
+
+  if (loggedIn) {
+    console.log(organization);
+    return <Redirect to={{
+      pathname: "/organization/profile",
+      state: { organization: organization },
+     }} />
+  } else if (navigateToCreation) {
+    // TODO: navigate to creation flow, pass auth token
+    return;
+  }
   return (
     <>
       {/* TODO: add Header component */}
-      <div style={styles.container}>
-        <Header isWhiteBackground />
-        <div style={styles.contentContainer}>
-          <Container maxWidth="lg">
-            <Grid container spacing={6}>
-              <Grid item xs>
-                <Box m={6}>
-                  <Box pb={5}>
-                    <Typography color="primary" variant="h1">
-                      How does this work?
-                    </Typography>
-                    <Typography color="primary" variant="subtitle1">
-                      If you are a hospital or a homeless shelter, help your
-                      community help you by crowd sourcing resources like
-                      medical supplies, food, and donations.
-                    </Typography>
-                  </Box>
-                  <Box pb={5}>
-                    <Typography color="primary" variant="h2">
-                      <PeopleOutlined fontSize="large" style={styles.icon} />{" "}
-                      Create a request profile
-                    </Typography>
-                    <Typography color="primary" variant="subtitle1">
-                      Create a profile requesting an amount of supplies from
-                      your community. We’ll first verify your organization, then
-                      publish your requests live.
-                    </Typography>
-                  </Box>
-                  <Box pb={8}>
-                    <Typography color="primary" variant="h2">
-                      <DriveEtaOutlined fontSize="large" style={styles.icon} />{" "}
-                      Receive drop-offs or pick-up donations
-                    </Typography>
-                    <Typography
-                      color="primary"
-                      variant="body1"
-                      style={styles.subTitle}
-                    >
-                      You can then either choose whether you would like to
-                      pick-up donations near you or receive scheduled drop-offs.
-                    </Typography>
-                  </Box>
-                  <Button size="large" variant="outlined" color="secondary">
-                    Create Profile
-                  </Button>
+      <Container maxWidth="lg">
+        <Grid container>
+          <Grid item xs lg={7}>
+            <Box m={6}>
+              <Box pb={5}>
+                <Typography color="textPrimary" variant="h1">
+                  How does this work?
+                </Typography>
+                <Typography color="textSecondary" variant="subtitle1">
+                  If you are a hospital or a homeless shelter, help your community help you by crowd sourcing resources like medical supplies, food, and donations.
+                </Typography>
+              </Box>
+              <Box pb={5}>
+                <Typography color="textPrimary" variant="h2">
+                  <PeopleOutlined fontSize="large" style={styles.icon}/>
+                  {' '}
+                  Create a request profile  
+                </Typography>
+                <Typography color="textSecondary" variant="subtitle1">
+                  Create a profile requesting an amount of supplies from your community. We’ll first verify your organization, then publish your requests live. 
+                </Typography>
+              </Box>
+              <Box pb={8}>
+                <Typography color="textPrimary" variant="h2">
+                  <DriveEtaOutlined fontSize="large" style={styles.icon}/>
+                  {' '}
+                  Receive drop-offs or pick-up donations
+                </Typography>
+                <Typography color="textSecondary" variant="body1" style={styles.subTitle}>
+                  You can then either choose whether you would like to pick-up donations near you or receive scheduled drop-offs.
+                </Typography>
+              </Box>
+              <Box style={styles.buttonBox}>
+                <Box style={styles.button}>
+                <GoogleLogin 
+                  clientId="650902157032-v1gqmd903sedgmdrpd0goa1343b049ug.apps.googleusercontent.com"
+                  onSuccess={onCreateAccountSuccess}
+                  onFailure={onCreateAccountFailure}
+                  cookiePolicy={'single_host_origin'}
+                  buttonText="Create account with Google"
+                  style={styles.button}
+                  theme="dark"
+                  responseType="code"
+                />
                 </Box>
-              </Grid>
-              <Grid item xs={5}>
-                <Box m={6}>
-                  <Paper variant="outlined">
-                    <Box p={3}>
-                      <Typography color="primary" variant="h2">
-                        Returning User
-                      </Typography>
-                      <TextField
-                        id="username"
-                        helperText="Username"
-                        fullWidth
-                        margin="normal"
-                        InputLabelProps={{
-                          shrink: true
-                        }}
-                        color="primary"
-                      />
-                      <TextField
-                        id="password"
-                        helperText="Password"
-                        fullWidth
-                        margin="normal"
-                        InputLabelProps={{
-                          shrink: true
-                        }}
-                      />
-                      <Box pt={2} style={{ textAlign: "right" }}>
-                        <Button
-                          size="large"
-                          variant="outlined"
-                          color="secondary"
-                        >
-                          Login
-                        </Button>
-                      </Box>
-                    </Box>
-                  </Paper>
-                </Box>
-              </Grid>
-            </Grid>
-          </Container>
-        </div>
-      </div>
+                <GoogleLogin 
+                  clientId="650902157032-v1gqmd903sedgmdrpd0goa1343b049ug.apps.googleusercontent.com"
+                  onSuccess={onLoginSuccess}
+                  onFailure={onLoginFailure}
+                  cookiePolicy={'single_host_origin'}
+                  buttonText="Sign in with Google"
+                />
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 }
