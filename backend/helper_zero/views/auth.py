6d76baf380ad458f1346705456a4a9b94ee5e1f3
vicky11z <vicky11zhang@gmail.com>
2020-03-29 10:22:51 -0700

    cleanup backend

--- backend/helper_zero/views/auth.py
+++ backend/helper_zero/views/auth.py
@@ -1,3 +1,4 @@
+import os
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 from google.oauth2 import id_token
@@ -7,10 +8,9 @@ from helper_zero.models import Organization, DonationRequest
 from helper_zero.serializers import OrganizationSerializer, DonationRequestSerializer
 
 class AuthView(viewsets.ViewSet):
-
   def list(self, request):
     auth_token = request.query_params.get('auth_token')
-    id_info = id_token.verify_oauth2_token(auth_token, requests.Request(), "650902157032-v1gqmd903sedgmdrpd0goa1343b049ug.apps.googleusercontent.com")
+    id_info = id_token.verify_oauth2_token(auth_token, requests.Request(), os.environ['GOOGLE_CLIENT_ID'])
     if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
       return Response(status=status.HTTP_401_UNAUTHORIZED)
     user_id = id_info['sub']
