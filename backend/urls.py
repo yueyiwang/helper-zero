c508487ff19a1a9fe1fb811043f5c965dabc013c
Annie Wang <anniew@yelp.com>
2020-03-29 01:26:35 -0700

    fe hook up?

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,7 @@
 # backend/urls.py
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
 from backend.helper_zero.views.organizations import OrganizationView
 from backend.helper_zero.views.users import UserView
@@ -9,6 +9,8 @@ from backend.helper_zero.views.donation_request import DonationRequestView
 from backend.helper_zero.views.donations import DonationView
 from backend.helper_zero.views.search import SearchView
 from backend.helper_zero.views.auth import AuthView
+from backend.helper_zero.views.frontend_app import FrontendAppView
+
 
 router = routers.DefaultRouter()
 router.register(r'login', AuthView, 'login')
@@ -21,4 +23,5 @@ router.register(r'donation_requests', DonationRequestView, 'donation_requests')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    re_path(r'^', FrontendAppView.as_view()),
 ]
