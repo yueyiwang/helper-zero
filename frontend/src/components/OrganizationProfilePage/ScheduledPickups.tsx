d39b3fe6f01688596a61a2a62910f4fea2c0d38b
vicky11z <vicky11zhang@gmail.com>
2020-03-29 16:31:35 -0700

    wip scheduled pickups

--- frontend/src/components/OrganizationProfilePage/ScheduledPickups.tsx
+++ frontend/src/components/OrganizationProfilePage/ScheduledPickups.tsx
@@ -1,13 +1,45 @@
 import React, { useState } from "react";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
+import { DonationType } from '../../types/DonationType';
 
 type Props = {
-
+  pickups: DonationType[];
 }
 
 const ScheduledPickups: React.FC<Props> = (props: Props) => {
   return (
     <>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Donator</TableCell>
+          <TableCell>Item</TableCell>
+          <TableCell>Quantity</TableCell>
+          <TableCell>Address</TableCell>
+          <TableCell>Contact Info</TableCell>
+          <TableCell>Scheduled Pickup Time</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {props.pickups.map((pickup: DonationType) => {
+          <TableRow>
+            <TableCell>{pickup.name}</TableCell>
+            <TableCell>{pickup.item_type}</TableCell>
+            <TableCell>{pickup.amount}</TableCell>
+            <TableCell>{pickup.name}</TableCell>
+            <TableCell>{pickup.name}</TableCell>
+            <TableCell>{pickup.name}</TableCell>
+          </TableRow>
+        })}
+      </TableBody>
+    </Table>
+
     </>
   )
 }
