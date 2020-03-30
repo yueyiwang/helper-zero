38304a266a5265eab6df020b112b090da3cafc64
Sonia Yang <sonia.yang@berkeley.edu>
2020-03-29 22:49:22 -0700

    Change index of to includes

--- frontend/src/components/OrganizationSignUpPage/OrganizationSignUpPage.tsx
+++ frontend/src/components/OrganizationSignUpPage/OrganizationSignUpPage.tsx
@@ -1,20 +1,23 @@
 // @ts-nocheck
-import React, {useState, useEffect} from "react";
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import React, { useState, useEffect } from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 import axios from "axios";
 import { Redirect } from "react-router-dom";
 
+import BasicInfoForm from "./Steps/BasicInfoForm";
+import DonationRequestForm from "./Steps/DonationRequestForm";
+import DonationMethodForm from "./Steps/DonationMethodForm";
+import ConfirmationPage from "./Steps/ConfirmationPage";
+import Header from "../Header";
+import { convertDataToOrg } from "../../utils";
 
-import BasicInfoForm from './Steps/BasicInfoForm';
-import DonationRequestForm from './Steps/DonationRequestForm';
-import DonationMethodForm from './Steps/DonationMethodForm';
-import ConfirmationPage from './Steps/ConfirmationPage';
-import Header from '../Header';
-import { convertDataToOrg } from '../../utils';
-
-import { DELIVERY_TYPE_DROP_OFF, DELIVERY_TYPE_PICK_UP, DELIVERY_TYPE_MAIL } from "../../constants";
+import {
+  DELIVERY_TYPE_DROP_OFF,
+  DELIVERY_TYPE_PICK_UP,
+  DELIVERY_TYPE_MAIL
+} from "../../constants";
 
 type Props = {
   location: {
@@ -28,34 +31,39 @@ const OrganizationSignUpPage: React.FC<Props> = (props: Props) => {
   const [formData, setFormData] = useState({});
   const [progress, setProgress] = useState(0);
   const [organization, setOrganization] = useState<OrganizationType>();
-  
-  const handleNext = async (data) => {
+
+  const handleNext = async data => {
     if (progress === 2) {
       const finalFormData = {
-        ...formData, ...data
-      }
+        ...formData,
+        ...data
+      };
       const organization: OrganizationType = {
         name: finalFormData.organizationName,
-        url: null, //not supported 
+        url: null, //not supported
         address: finalFormData.address,
-        description: null, //not supported 
+        description: null, //not supported
         phone: finalFormData.phone,
         org_type: finalFormData.organizationType,
         email: finalFormData.email,
-        is_pickup: finalFormData.methods.indexOf(DELIVERY_TYPE_PICK_UP) > 0,
-        is_dropoff: finalFormData.methods.indexOf(DELIVERY_TYPE_DROP_OFF) > 0,
-        is_mail: finalFormData.methods.indexOf(DELIVERY_TYPE_MAIL) > 0,
+        is_pickup: finalFormData.methods.includes(DELIVERY_TYPE_PICK_UP) > 0,
+        is_dropoff: finalFormData.methods.includes(DELIVERY_TYPE_DROP_OFF) > 0,
+        is_mail: finalFormData.methods.includes(DELIVERY_TYPE_MAIL) > 0,
         pickup_instructions: finalFormData[DELIVERY_TYPE_PICK_UP].instruction,
         dropoff_instructions: finalFormData[DELIVERY_TYPE_DROP_OFF].instruction,
         mail_instructions: finalFormData[DELIVERY_TYPE_MAIL].instruction,
         zipcode: "94114", //TODO: need address -> geocode
         lat: "1.3",
         lon: "2.0",
-        pickup_times: JSON.stringify(finalFormData[DELIVERY_TYPE_MAIL].times),
-        dropoff_times: JSON.stringify(finalFormData[DELIVERY_TYPE_DROP_OFF].times),
-        auth_token: props.location.state.authToken,
-      }
-      axios.post('/api/organizations/', organization).then((resp) => {
+        pickup_times: JSON.stringify(
+          finalFormData[DELIVERY_TYPE_PICK_UP].times
+        ),
+        dropoff_times: JSON.stringify(
+          finalFormData[DELIVERY_TYPE_DROP_OFF].times
+        ),
+        auth_token: props.location.state.authToken
+      };
+      axios.post("/api/organizations/", organization).then(resp => {
         if (resp.status != 200) {
           console.log(resp);
         }
@@ -73,62 +81,63 @@ const OrganizationSignUpPage: React.FC<Props> = (props: Props) => {
               item: itemName,
               item_type: donationType,
               amount_requested: Number(itemCount),
-              amount_received: 0,
+              amount_received: 0
             };
-            axios.post('/api/donation_requests/', donationRequest)
-              .then((resp) => {
+            axios
+              .post("/api/donation_requests/", donationRequest)
+              .then(resp => {
                 if (resp.status != 200) {
                   console.log(resp);
                 }
-              })
+              });
           });
-        })
+        });
       });
+    } else {
+      setFormData({ ...formData, ...data });
+      setProgress(progress + 1);
     }
-    else {
-      setFormData({...formData, ...data});
-      setProgress(progress+1);
-    }
-  }
+  };
 
   const handleBack = () => {
-    setProgress(progress-1);
-  }
+    setProgress(progress - 1);
+  };
 
   if (organization != undefined) {
     return (
-    <Redirect 
-      to={{
-        pathname: "/organization/confirmation",
-        state: { organization: organization, authToken: props.location.state.authToken }
-      }}
+      <Redirect
+        to={{
+          pathname: "/organization/confirmation",
+          state: {
+            organization: organization,
+            authToken: props.location.state.authToken
+          }
+        }}
       />
-    )
+    );
   }
 
   return (
     <>
       <Header isWhiteBackground={true} />
-      <Container maxWidth="lg" style={{"padding": "100px"}}>
+      <Container maxWidth="lg" style={{ padding: "100px" }}>
         <Box m={6}>
-          <Typography variant="h1">
-            Create Profile
-          </Typography>
+          <Typography variant="h1">Create Profile</Typography>
           {/* TODO Progress Bar */}
 
           {progress === 0 && (
-            <BasicInfoForm onNext={handleNext} onBack={handleBack}/>
+            <BasicInfoForm onNext={handleNext} onBack={handleBack} />
           )}
           {progress === 1 && (
-            <DonationRequestForm onNext={handleNext} onBack={handleBack}/>
+            <DonationRequestForm onNext={handleNext} onBack={handleBack} />
           )}
           {progress === 2 && (
-            <DonationMethodForm onNext={handleNext} onBack={handleBack}/>
+            <DonationMethodForm onNext={handleNext} onBack={handleBack} />
           )}
         </Box>
       </Container>
     </>
   );
-}
+};
 
 export default OrganizationSignUpPage;
